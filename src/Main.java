132b39d7220ae6701907d1ce1ae1f7efa4575ffd
4milo <4milov@gmail.com>
2022-09-03 02:38:54 +0300

    add method makeNegative2 with Math.abs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import static java.lang.Math.abs;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(makeNegative(10));
+        System.out.println(makeNegative(9));
     }
     public static int makeNegative(final int x) {
         return x <= 0 ? x : x * (-1);
+    }
+
+    public static int makeNegative2(final int x) {
+        return -abs(x);
     }
 }
